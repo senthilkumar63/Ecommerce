c757754550decdf29864d7404c5f4b842f8b294c
senthilkumar63 <127287380+senthilkumar63@users.noreply.github.com>
2023-03-09 12:16:37 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+/* eslint-disable react/jsx-no-undef */
+import React from "react";
+import Body from "./Body/Body";
+import Slider from "./Component/Slider";
+import Footer from "./Footer/Footer";
+
+function App() {
+  return (
+    <>
+     <Body />
+     <Slider />
+      <Footer />
+     
+    </>
+   
+  );
+}
+
+export default App;
