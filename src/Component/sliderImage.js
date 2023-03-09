66a2ec740cec10aaf3716d2b95076e04f86a796a
senthilkumar63 <127287380+senthilkumar63@users.noreply.github.com>
2023-03-09 12:13:11 +0530

    Create sliderImage.js

---
+++ src/Component/sliderImage.js
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-anonymous-default-export */
+import First from "../assets/First.jpeg";
+import Second from "../assets/Second.jpeg";
+import Third from "../assets/Third.jpeg";
+import Fourth from "../assets/Fourth.jpeg";
+import Fifth from "../assets/Fifth.jpeg";
+import Sixth from "../assets/Sixth.jpeg";
+import Seventh from "../assets/Seventh.jpeg";
+
+export default [
+  {
+    urls: First,
+  },
+  {
+    urls: Second,
+  },
+  {
+    urls: Third,
+  },
+  {
+    urls: Fourth,
+  },
+  {
+    urls: Fifth,
+  },
+  {
+    urls: Sixth,
+  },
+  {
+    urls: Seventh,
+  },
+];
